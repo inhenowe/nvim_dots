f620803ced22f422d8cb1bcfb0802e647f726456
ale <aleorgo11@gmail.com>
2026-03-06 18:19:03 +0100

    comments

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
+--[[
+	Configuracion de nvim simple con plugins minimos
+	y estilo minimalista.
+	Realizado por={
+		github := inhenowe,
+		gmail := inhenowe@gmail.com,
+	}
+]]--
 
+--| Atajos de teclado, funciones y config de vim |--
 require("core.options")
 require("core.keymaps")
 require("core.functions")
-
+--| Plugins y configuraciones de plugins |--
 require("plugins.plugins")
 
